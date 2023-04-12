75536fd9a5e4e4f8680a7fe4daf779e7d986e00a
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-11 22:45:26 -0300

    Criando pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonFooter, IonText } from "@ionic/react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton></IonMenuButton>
+                    </IonButtons>
+                    <IonTitle>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent></IonContent>
+
+            <IonFooter>
+              <IonToolbar>
+                <IonText>Rodape</IonText>
+              </IonToolbar>
+            </IonFooter>
+        </IonPage>
+    );
 }
