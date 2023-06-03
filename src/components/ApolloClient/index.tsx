20dec4aff478f06252422be854b7fe7f31fb2cf1
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-06-02 23:03:28 -0300

    ...

--- src/components/ApolloClient/index.tsx
+++ src/components/ApolloClient/index.tsx
@@ -2,8 +2,9 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ReactElement } from "react";
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "http://localhost:4000/",
-    // uri: "https://api-mkb.herokuapp.com/",
+    uri: "http://localhost:4000/", // API local
+    // uri: "http://192.168.100.106:4000/", // API mobile
+    // uri: "https://api-mkb.herokuapp.com/", // API heroku
 });
 type Props = {
     children: ReactElement;
